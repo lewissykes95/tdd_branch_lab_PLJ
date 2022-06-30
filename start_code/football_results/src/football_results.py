3e6a11e757c7c7c1c63416541cf3bc73facf940c
Lewis <lewisdsykes@gmail.com>
2022-06-30 13:11:05 +0100

    added list to football_results file

---
+++ start_code/football_results/src/football_results.py
@@ -0,0 +1,12 @@
+football_results = {    
+    "home_score": 0,
+    "away_score": 1
+}
+
+
+def get_result(final_score):
+    
+
+def get_results(final_scores):
+    pass
+    # (You could try and use a list comprehension for this)
