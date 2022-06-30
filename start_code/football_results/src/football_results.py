4405452859787ae6dc0efd283b30ed35244b380e
Lewis <lewisdsykes@gmail.com>
2022-06-30 13:54:12 +0100

    ran 3 tests and all passed for football_scores

--- start_code/football_results/src/football_results.py
+++ start_code/football_results/src/football_results.py
@@ -1,12 +1,14 @@
-football_results = {    
-    "home_score": 0,
-    "away_score": 1
-}
-
 
 def get_result(final_score):
+    if final_score["home_score"] < final_score["away_score"]:
+        return "away win"
+    elif final_score["home_score"] > final_score["away_score"]:
+        return "home win"
+    else: 
+        return "draw"
+    
     
 
-def get_results(final_scores):
-    pass
-    # (You could try and use a list comprehension for this)
+# def get_results(final_scores):
+#     pass
+#     # (You could try and use a list comprehension for this)
--- start_code/football_results/tests/football_results_test.py
+++ start_code/football_results/tests/football_results_test.py
@@ -2,9 +2,33 @@ import unittest
 from src.football_results import *
 
 class FootballResultsTest(unittest.TestCase):
+
+
+    def setUp(self):
+        self.football_results1 = {    
+                "home_score": 0,
+                "away_score": 1
+                }   
+        self.football_results2 = {    
+                "home_score": 1,
+                "away_score": 0
+                }   
+        self.football_results3 = {    
+                "home_score": 1,
+                "away_score": 1
+                }   
     
     def test_get_right_result(self):
-        self.assertEqual()
+        result = get_result(self.football_results1)
+        self.assertEqual("away win", result)
+
+    def test_get_right_result2(self):
+        result = get_result(self.football_results2)
+        self.assertEqual("home win", result)
+
+    def test_get_right_result3(self):
+        result = get_result(self.football_results3)
+        self.assertEqual("draw", result)
     
     # Test we get the right result string for a final score dictionary representing -
 
